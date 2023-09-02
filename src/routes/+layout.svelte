938411a8cb2bb990b46665773e1b5c9364208b12
Till Wittenburg <wittenburg.till+git@gmail.com>
2023-09-03 01:02:22 +0200

    chore: Move Header and Sidebar to Layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,22 @@
 
     import {initFlowbite} from 'flowbite'
     import {onMount} from "svelte";
+    import Sidebar from "$lib/components/sidebar/Sidebar.svelte";
+    import Header from "$lib/components/Header.svelte";
 
     onMount(initFlowbite)
 
 </script>
 
-<slot/>
+
+<div class="bg-gray-50 dark:bg-gray-900">
+
+    <Header/>
+
+    <Sidebar/>
+
+    <main class="p-4 md:ml-64 h-auto min-h-screen pt-20">
+        <slot/>
+    </main>
+</div>
+
