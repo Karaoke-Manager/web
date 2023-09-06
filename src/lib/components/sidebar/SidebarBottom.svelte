7522ebdaa42c84264320db85faa3b0d0e863459f
Till Wittenburg <wittenburg.till+git@gmail.com>
2023-09-06 20:03:49 +0200

    chore: Refactor ThemeToggle

--- src/lib/components/sidebar/SidebarBottom.svelte
+++ src/lib/components/sidebar/SidebarBottom.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import SidebarLink from "$lib/components/sidebar/SidebarLink.svelte";
-    import ThemeToggle from "$lib/components/ThemeToggle.svelte";
+    import ThemeToggle from "$lib/components/sidebar/ThemeToggle.svelte";
 </script>
 
 <div class="absolute bottom-0 left-0 justify-center p-4 space-x-4 w-full flex bg-white dark:bg-gray-800 z-20">
